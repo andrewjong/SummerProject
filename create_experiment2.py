43ea0a84c42dfc541255eb33ad7ab376926afc41
atticusg <atticusg@gmail.com>
2018-08-08 16:13:53 -0700

    ajksd

--- create_experiment2.py
+++ create_experiment2.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     test_size = 10000
     data, _, _ = gd.process_data(1.0)
     examples = gd.generate_balanced_data("simple_solutions", "boolean_solutions", 0,train_size, data, simple_sampling = "level 2", boolean_sampling = "level 1")
-    gd.save_data(examples, "experiment2_level10.train")
+    gd.save_data(examples, "experiment2_level21.train")
     examples = gd.generate_balanced_data("simple_solutions", "boolean_solutions", 0,val_size, data, simple_sampling = "level 2", boolean_sampling = "level 1")
-    gd.save_data(examples, "experiment2_level10.val")
+    gd.save_data(examples, "experiment2_level21.val")
     examples = gd.generate_balanced_data("simple_solutions", "boolean_solutions", 0,test_size, data, simple_sampling = "level 2", boolean_sampling = "level 1")
-    gd.save_data(examples, "experiment2_level10.test")
+    gd.save_data(examples, "experiment2_level21.test")
