d65f10f7919e0b89c8020133e4de6a3a5b0ca833
atticusg <atticusg@gmail.com>
2019-01-31 19:04:25 -0800

    genarlization data generated

--- generate_data.py
+++ generate_data.py
@@ -568,21 +568,21 @@ def create_corpus(size, filename):
     data, _, _ = process_data(1.0)
     examples = generate_balanced_data("simple_solutions", "boolean_solutions", size, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0")
     save_data(examples, filename)
-
-data, _, _ = process_data(1.0)
-restrictions, inverse_restrictions = nlm.create_gen_split()
-examples = generate_balanced_data("simple_solutions", "boolean_solutions", 50, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = restrictions)
-save_data(examples, "dummy.train")
-examples = generate_balanced_data("simple_solutions", "boolean_solutions", 10, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = inverse_restrictions)
-save_data(examples, "dummy.val")
-examples = generate_balanced_data("simple_solutions", "boolean_solutions", 10, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = inverse_restrictions)
-save_data(examples, "dummy.test")
-premise = parse_simple_sentence(data,examples[0][0])[0]
-hypothesis = parse_simple_sentence(data,examples[0][2])[0]
-_, relations_seen = nlm.compute_simple_relation_gentest(premise, hypothesis)
-for example in examples:
-    premise = parse_simple_sentence(data,example[0])[0]
-    hypothesis = parse_simple_sentence(data,example[2])[0]
-    _, relations_seen = nlm.compute_simple_relation_gentest(premise, hypothesis, relations_seen)
-for k in relations_seen:
-    print(k, len(relations_seen[k]))
+if True:
+    data, _, _ = process_data(1.0)
+    restrictions, inverse_restrictions = nlm.create_gen_split()
+    examples = generate_balanced_data("simple_solutions", "boolean_solutions", 500000, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = restrictions)
+    save_data(examples, "gendata.train")
+    examples = generate_balanced_data("simple_solutions", "boolean_solutions", 10000, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = inverse_restrictions)
+    save_data(examples, "gendata.val")
+    examples = generate_balanced_data("simple_solutions", "boolean_solutions", 10000, 0, data, simple_sampling = "level 2", boolean_sampling = "level 0",restrictions = inverse_restrictions)
+    save_data(examples, "gendata.test")
+    premise = parse_simple_sentence(data,examples[0][0])[0]
+    hypothesis = parse_simple_sentence(data,examples[0][2])[0]
+    _, relations_seen = nlm.compute_simple_relation_gentest(premise, hypothesis)
+    for example in examples:
+        premise = parse_simple_sentence(data,example[0])[0]
+        hypothesis = parse_simple_sentence(data,example[2])[0]
+        _, relations_seen = nlm.compute_simple_relation_gentest(premise, hypothesis, relations_seen)
+    for k in relations_seen:
+        print(k, len(relations_seen[k]))
--- train.py
+++ train.py
@@ -7,8 +7,9 @@ import math
 import numpy as np
 from model import PIModel
 
-class config:
+class configuration:
     def __init__(self, attention, learning_rate, l2_norm, dropout, activation_function, vocab_dim, state_size, batch_size):
+        self.retrain_embeddings = True
         self.vocab_dim = vocab_dim
         self.attention = attention
         self.max_prem_len = 9
@@ -16,7 +17,7 @@ class config:
         self.dropout = dropout
         self.state_size = state_size
         self.max_grad_norm = 5.
-        self.batch_size = batch_sizes
+        self.batch_size = batch_size
         self.l2_norm = l2_norm
         self.learning_rate= learning_rate
         self.activationstring = activation_function
@@ -37,21 +38,21 @@ def confuse(preds, labels):
         matrix[preds[i]][labels[i]] += 1
     return matrix
 
-def hyperparameter_search(models, attentions, data_path, learning_rates, l2_norms, dropouts, activation_functions, vocab_dims, state_sizes, batch_sizes, num_epochs):
+def hyperparameter_search(models,  data_path, learning_rates, l2_norms, dropouts, activation_functions, vocab_dims, state_sizes, batch_sizes, num_epoch):
     embeddings = model_util.get_word_vec(vocab=model_util.get_vocab())
     word_to_id = model_util.get_word_to_id(vocab=model_util.get_vocab())
     labels = ['entailment','contradiction','neutral']
     results = dict()
     hyp_results = dict()
-    for model in models:
+    for model,attention in models:
         for learning_rate in learning_rates:
             for l2_norm in l2_norms:
                 for dropout in dropouts:
                     for activation_function in activation_functions:
                         for vocab_dim in vocab_dims:
-                            for state_size in state sizes
+                            for state_size in state_sizes:
                                 for batch_size in batch_sizes:
-                                    config = config(attention, learning_rate, l2_norm, dropout, activation_function, vocab_dim, state_size, batch_size)
+                                    config = configuration(attention, learning_rate, l2_norm, dropout, activation_function, vocab_dim, state_size, batch_size)
                                     m = PIModel(config, embeddings, model)
                                     with tf.Session() as sess:
                                         tf.global_variables_initializer().run()
@@ -59,17 +60,18 @@ def hyperparameter_search(models, attentions, data_path, learning_rates, l2_norm
                                         results_folder = ""
                                         for param in params:
                                             results_folder += param
+                                        os.mkdir(results_folder)
                                         hypaccs= []
                                         results = []
                                         best_val = 0
                                         best_test = 0
                                         count = 0
-                                        for ep in range(int((config.num_epoch*500000)/train_size)):
-                                            print("\n>> Beginning epoch {}/{} <<".format(ep, config.num_epoch))
+                                        for ep in range(num_epoch):
+                                            print("\n>> Beginning epoch {}/{} <<".format(ep, num_epoch))
                                             ### Call training and validation routine
                                             ### This part runs one epoch of trainin#g and one epoch of validation
                                             ### Outcomes: preds, labels, constr, loss
-                                            train_data = model_util.get_feed(os.path.join(data_path+  ".train"), config.batch_size, word_to_id, config.max_prem_len, config.max_hyp_len,num_iter=int(train_size/config.batch_size), shuffle = True)
+                                            train_data = model_util.get_feed(os.path.join(data_path+  ".train"), config.batch_size, word_to_id, config.max_prem_len, config.max_hyp_len,num_iter=int(500000/config.batch_size), shuffle = True)
                                             for prem, prem_len, hyp, hyp_len, label in train_data:
                                                 pred, _ = m.optimize(sess, prem, prem_len, hyp, hyp_len, label)
                                                 count += 1
@@ -84,11 +86,11 @@ def hyperparameter_search(models, attentions, data_path, learning_rates, l2_norm
                                                         best_train= ratio(preds_train, labels_train)
                                                         saver = tf.train.Saver()
                                                         save_path = saver.save(sess, os.path.join(results_folder, "bestmodel.ckpt"))
-                                                    hypaccs.append((ratio(preds_val,labels_val),ratio(preds_test, labels_test))
+                                                    hypaccs.append((ratio(preds_val,labels_val),ratio(preds_test, labels_test)))
                                         hyp_results[params] = best_val
                                 for k in hyp_results:
                                     print(k, ":    ",hyp_results[k])
                                 with open(model + "hypsearch_results", "w") as f:
                                     f.write(json.dumps())
 if __name__ == '__main__':
-    print("hello")
+    hyperparameter_search([("seq2seq","no")], "training_data/dummy", [0.001], [0.001], [1.0], ["tanh"], [100], [100], [64], 2)
